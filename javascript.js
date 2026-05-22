498815ade7fe35f83f0a83fe2cb3ca583ae10adb
Edwin <264506916+m-redw@users.noreply.github.com>
2026-05-22 14:18:41 -0700

    Create "operate" function

--- javascript.js
+++ javascript.js
@@ -17,3 +17,17 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(op, num1, num2) {
+    if (op === '+' || op === 'add') {
+        return add(num1, num2);
+    } else if (op === '-' || op === 'subtract') {
+        return subtract(num1, num2);
+    } else if (op === 'x' || op === 'multiply') {
+        return multiply(num1, num2);
+    } else if (op === '/' || op === 'divide') {
+        return divide(num1, num2);
+    } else {
+        return 'Please provide appropriate operator';
+    }
+}
