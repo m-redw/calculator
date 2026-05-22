d27b3db0885435da1e7013265fd57dc071beb1c4
Edwin <264506916+m-redw@users.noreply.github.com>
2026-05-22 15:15:23 -0700

    Make functioning calculator

--- javascript.js
+++ javascript.js
@@ -32,15 +32,55 @@ function operate(op, num1, num2) {
     }
 }
 
-function updateDisplay() {
+function updateDisplay(text) {
     const display = document.querySelector('.display');
-    display.textContent = `${number1} ${operator} ${number2}`;
+    display.textContent = text;
+}
+
+function resetCalc() {
+    number1 = '';
+    number2 = '';
+    operator = '';
 }
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        number1 = button.textContent;
-        updateDisplay();
+        const nonNumbers = '+-x/=';
+        const isNumber = !nonNumbers.includes(button.textContent);
+        if (isNumber) {
+            console.log(button.textContent)
+            if (button.textContent === 'AC') {
+                resetCalc();
+                updateDisplay('');
+                return;
+            }
+
+            if (operator != '') {
+                number2 = button.textContent;
+                updateDisplay(`${number1} ${operator} ${number2}`);
+            } else {
+                number1 = button.textContent;
+                updateDisplay(`${number1}`);
+            }
+        } else if (button.textContent === '=') {
+            const isOperationReady = (number1 != '' && number2 != '' && operator != '');
+            if (isOperationReady) {
+                const numNumber1 = parseInt(number1);
+                const numNumber2 = parseInt(number2);
+                const answer = operate(operator, numNumber1, numNumber2);
+                updateDisplay(String(answer));
+                resetCalc();
+            } else {
+                alert('Boi, ts (this) calc (calculator) needs 2 nums and an operator!');
+            }
+        } else {
+            if (number1 != '') {
+                operator = button.textContent;
+                updateDisplay(`${number1} ${operator} ${number2}`);
+            } else {
+                alert('Boi, ts (this) calc (calculator) needs a NEW number first! NEW as in the old answer doesn\'t count!');
+            }
+        }   
     });
 });
